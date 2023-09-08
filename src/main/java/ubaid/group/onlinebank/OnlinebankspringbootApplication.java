ddd04ce0aece20a984ec866f261ed633ec6fe132
unknown <ubaiddd05@gmail.com>
2023-09-08 14:15:43 +0530

    first commit

---
+++ src/main/java/ubaid/group/onlinebank/OnlinebankspringbootApplication.java
@@ -0,0 +1,13 @@
+package ubaid.group.onlinebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankspringbootApplication.class, args);
+	}
+
+}
