b24604d89b408af609856f684f369b4e1260aca0
unknown <ubaiddd05@gmail.com>
2023-09-08 14:41:05 +0530

    second commit

--- src/main/java/ubaid/group/onlinebank/OnlinebankspringbootApplication.java
+++ src/main/java/ubaid/group/onlinebank/OnlinebankspringbootApplication.java
@@ -2,12 +2,16 @@ package ubaid.group.onlinebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class OnlinebankspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlinebankspringbootApplication.class, args);
-	}
+		System.out.println("hello");
+		}
 
 }
